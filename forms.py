2e965e915573555b4dd1cb428f4737c9fc70ed4b
SebastianBas <134089498+SebastianBas@users.noreply.github.com>
2024-01-15 20:57:18 -0500

    hola

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField
+from flask import Markup
+
 
 class LanguageForm(FlaskForm):
     language = SelectField('Language', choices=[ ('es', 'Español') , ('en', 'English')])
